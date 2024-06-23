fd57a18e73bd6c40fb34a4d654ee5c96fc88a9ad
Asghar Nazir <asghar@syntaxtechs.com>
2024-06-23 14:29:37 -0400

    DB utils added

--- src/test/java/utils/ConfigReader.java
+++ src/test/java/utils/ConfigReader.java
@@ -5,10 +5,20 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class ConfigReader {
-    public static String read(String key) throws IOException {
-        FileInputStream fis=new FileInputStream(Constants.CONFIG_FILE_PATH);
+    public static String read(String key,String path)  {
         Properties properties=new Properties();
-        properties.load(fis);
+        try(FileInputStream fis=new FileInputStream(path)){
+            properties.load(fis);
+        }catch (IOException fe){
+            fe.printStackTrace();
+        }
+
         return properties.getProperty(key);
+    }
+
+    public static String read(String key)  {
+
+        return  read(key,Constants.CONFIG_FILE_PATH);
+
     }
 }
---
+++ src/test/java/utils/DBUtils.java
@@ -0,0 +1,58 @@
+package utils;
+
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+public class DBUtils {
+
+
+    public static void main(String[] args) {
+        System.out.println( fetch("select * from person"));
+    }
+
+    public static List<Map<String,String>> fetch(String query){
+
+
+
+        List<Map<String,String>> tableData=new ArrayList<>();
+        try {
+            // creates a connection to the database
+            Connection connection= DriverManager.getConnection(ConfigReader.read("dbURL"),ConfigReader.read("dbUsername"),ConfigReader.read("dbPassword"));
+        /*
+        take the queries from java code execute them on the database and bring the results back
+        into your java program.
+         */
+            Statement statement=connection.createStatement();
+            // Resultset contains the actual data from tables
+            ResultSet resultset= statement.executeQuery(query);
+            ResultSetMetaData rsm=resultset.getMetaData();
+
+
+            //next() checks if there are any more rows in the ResultSet if yes it returns true
+            // and moves the cursor to the next row
+            while(resultset.next()){
+                Map<String,String> rowMap=new LinkedHashMap<>();
+                for (int i = 1; i <=rsm.getColumnCount() ; i++) {
+                    //getting the column names from resultsetmetadata
+                    String key=rsm.getColumnLabel(i);
+                    //getting the actual data from rs
+                    String value=resultset.getString(i);
+                    rowMap.put(key,value);
+                }
+                tableData.add(rowMap);
+
+            }
+
+
+            // Break till 11:52
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return tableData;
+
+    }
+}
