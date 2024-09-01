b8525e919bca2501194bb095122271afc8b92bf7
sohai <SyntaxTechnologies>
2024-09-01 18:20:05 +0530

    hl

--- src/test/java/runners/ReviewRunner.java
+++ src/test/java/runners/ReviewRunner.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features/",
         glue = "steps",
         dryRun = false,
-        tags = "@datadriven"
+        tags = "@datadriven321"
 )
 
 public class ReviewRunner {
